6266a5e82dcc65f6d840b43bb617fa76f2a36343
DBombay <d.bombay@live.com>
2017-02-28 17:01:55 -0500

    Third User Story complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,32 @@
 class QuestionsController < ApplicationController
   def index
-    @questions = Question.all
+    @questions = Question.list
+    @question = Question.new
   end
 
   def show
     @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @questions = Question.list
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to questions_path, notice: 'You successfully posted a new question.'
+    else
+      render action: 'index'
+    end
+  end
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ApplicationRecord
 
   validates_length_of :title, :minimum => 40
   validates_length_of :description, :minimum => 150
+
+  def self.list
+    all.order(created_at: :desc)
+  end
 end
